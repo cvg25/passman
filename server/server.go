f38d266d968b19c5ab87820c9902290183d0c3a6
Carlos Vélez García <cvg25@alu.ua.es>
2019-02-26 10:16:16 +0100

    Servidor funcional http

--- server/server.go
+++ server/server.go
@@ -1,7 +1,16 @@
 // Servidor
-
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there!")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
