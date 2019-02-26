12c2ac0e251451f396337c4a0fbec712aeb137e8
Airam Perez Catala <apc118@alu.ua.es>
2019-02-26 10:47:53 +0100

    Conexion con el servidor HTTPS

--- client/client.go
+++ client/client.go
@@ -2,6 +2,27 @@
 
 package main
 
-func main() {
+import (
+	"crypto/tls"
+	"fmt"
+)
 
+func chk(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	// Configuracion de la conexion
+	conf := &tls.Config{
+		// InsecureSkipVerify: true,
+	}
+
+	// Conexion con el servidor
+	conn, err := tls.Dial("tcp", "localhost:1337", conf)
+	chk(err)
+	defer conn.Close()
+
+	fmt.Println("Conectado a ", conn.RemoteAddr())
 }
